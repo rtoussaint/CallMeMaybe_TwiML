173a0a53cfef57e526d237f2800a83e847755573
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-09-13 05:32:58 -0400

    reduced max length

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var app = express();
 
+// queries: tonesSoFar, number
 app.get('/', function(req, res){
     var play = '';
 
-    if(req.query && req.query.tonesSoFar && req.query.tonesSoFar.length > 0){
+    if (req.query.tonesSoFar && req.query.tonesSoFar.length > 0){
         play = '<Play digits="ww' + req.query.tonesSoFar.split('').join('ww') + '" > </Play>';
+    } else {
+        return res.send(400);
+    }
+
+    if (!req.query.number) {
+        return res.send(400);
     }
 
     var output = '<?xml version="1.0" encoding="UTF-8"?><Response>' +
     play +
-    '<Record maxLength="100" timeout="4" transcribe="true" transcribeCallback="' + process.env.CALLBACK_HOST +
-    '?tonesSoFar='+req.query.tonesSoFar + '" ' +
+    '<Record maxLength="30" timeout="4" transcribe="true" transcribeCallback="' + process.env.CALLBACK_HOST +
+    '?tonesSoFar='+req.query.tonesSoFar + '&number='+req.query.number+'" ' +
     'action="'+process.env.CALLBACK_HOST+'"/></Response>';
     res.send(output);
     console.log(output);
